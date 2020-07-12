d9406f38d4ecb6ced90656d90b7241c1495973bf
Ice <richard.jun.noh@gmail.com>
2020-07-12 05:47:43 -0500

    Set most menu ready, next commit will remove text
    The input text for the different parameters is too much work right now.
    Just set the 2 different things.

--- Scripts/UI Buttons/MoveObjects.cs
+++ Scripts/UI Buttons/MoveObjects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,16 +13,29 @@ public class MoveObjects : MonoBehaviour
     private bool isBeingHeld = false;
 
     private static bool isSelecting = false;
-    private static GameObject selectedGameObject = null;
+    private static GameObject selectedGameObject = null; // for connecting
 
-    public static GameObject selectionMenuObject;
+    public static GameObject selectionMenuObject = null;
+    public static ToFromSelection currentConnectionSelection;
+    
 
-    public static bool[] indicesOfInactiveMenuObjects;
-
-    void Start() 
+    public enum ToFromSelection 
     {
-        selectionMenuObject = GameObject.FindWithTag("UI-SelectionMenu");
-        indicesOfInactiveMenuObjects = new bool[4];
+        NOT_SELECTED = 0,
+        TO = 1,
+        FROM = 2
+    }
+
+
+    void Start()
+    {
+        if (selectionMenuObject == null)
+        {
+            selectionMenuObject = GameObject.FindWithTag("UI-SelectionMenu");
+            UnityEngine.Debug.Log("Obtained selectionMenuObject: " + selectionMenuObject);
+            selectionMenuObject.SetActive(false);
+        }
+
     }
 
     void Update()
@@ -35,6 +49,7 @@ public class MoveObjects : MonoBehaviour
         }
     }
 
+    //Used for moving objects and selecting connections
     private void OnMouseDown()
     {
         //Left mouse click selection
@@ -42,6 +57,7 @@ public class MoveObjects : MonoBehaviour
         {
             if (!isSelecting)
             {
+
                 UnityEngine.Debug.Log("Left mouse click is pressed");
                 Vector3 mousePos;
                 mousePos = Input.mousePosition;
@@ -52,8 +68,33 @@ public class MoveObjects : MonoBehaviour
 
                 isBeingHeld = true;
             }
-            else //choosing game object TODO
-            { }
+            else
+            {
+                if (currentConnectionSelection == ToFromSelection.TO)
+                    selectedGameObject.GetComponent<AbstractSimForce>().o1 = this.gameObject;
+                else if (currentConnectionSelection == ToFromSelection.FROM)
+                    selectedGameObject.GetComponent<AbstractSimForce>().o2 = this.gameObject;
+
+            }
+            //reset menu and variables TODO
+            isSelecting = false;
+            selectedGameObject = null;
+            currentConnectionSelection = ToFromSelection.NOT_SELECTED;
+
+            foreach (Transform selectionMenuItem in selectionMenuObject.transform)
+            {
+                GameObject childGameObject = selectionMenuItem.gameObject;
+                childGameObject.SetActive(true);
+                if (childGameObject.CompareTag("UI-SelectionMenuInputField"))
+                {
+                    Transform placeHolderTransform = selectionMenuItem.GetChild(0);
+                    if (!placeHolderTransform.gameObject.CompareTag("UI-SelectionMenuInputPlaceholder"))
+                        placeHolderTransform = selectionMenuItem.GetChild(1);
+                    
+                    placeHolderTransform.GetComponent<Text>().text = "reset";
+                }
+            }
+            selectionMenuObject.SetActive(false);
         }
     }
 
@@ -62,17 +103,22 @@ public class MoveObjects : MonoBehaviour
         isBeingHeld = false;
     }
 
-    private void OnMouseOver() 
+    //Used right clicking configuration selection menu
+    private void OnMouseOver()
     {
         //Right mouse click selection
         if (Input.GetMouseButtonDown(1))
         {
-
             Vector3 mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3 gameWorldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
             //this.gameObject
             UnityEngine.Debug.Log("Right mouse click is pressed and this object: \n" + this.gameObject + " is selected.");
+            
+            selectionMenuObject.SetActive(true);
+            selectionMenuObject.transform.position = mousePos;
+
+            UnityEngine.Debug.Log("Is selection menu active?: " + selectionMenuObject.activeSelf);
 
             var thisGameObject = this.gameObject;
             if (thisGameObject.CompareTag("External Forces"))
@@ -86,55 +132,81 @@ public class MoveObjects : MonoBehaviour
     }
 
     //only 1 value, and the 2 connectors
-    private void ConfigureExternalForcesMenu() 
+    private void ConfigureExternalForcesMenu()
     {
-        selectionMenuObject.SetActive(true);
         Transform selectionMenuTransform = selectionMenuObject.transform;
-        
-        //Set InputField1 inactive, keep track of it for reactivation
+
+        //Set InputField1 inactive
         selectionMenuTransform.GetChild(2).gameObject.SetActive(false); //this is InputField1
-        indicesOfInactiveMenuObjects[2] = true;
 
         //Set placeholder text of input
-        GameObject inputField0bject = selectionMenuTransform.GetChild(1);
-        GameObject placeholderObject = inputFieldObject.GetChild(0);
-        placeholderObject.GetComponent<Text>().text = "Force (N)";
+        Transform inputFieldObject0 = selectionMenuTransform.GetChild(1);
+        GameObject placeholderObject0 = inputFieldObject0.GetChild(0).gameObject;
+
+        //At creation, the inputfield generates a child GameObject at index 0 called InputField0 Input Caret
+        if (!placeholderObject0.CompareTag("UI-SelectionMenuInputPlaceholder"))
+            placeholderObject0 = inputFieldObject0.GetChild(1).gameObject;
+        placeholderObject0.GetComponent<Text>().text = "Force (N)";
 
     }
 
     //only 1 value
-    private void ConfigureMassCubeMenu() 
+    private void ConfigureMassCubeMenu()
     {
-        selectionMenuObject.SetActive(true);
         Transform selectionMenuTransform = selectionMenuObject.transform;
 
         //Set InputField1 and ConnectorButtons inactive, keep track of it for reactivation
         selectionMenuTransform.GetChild(2).gameObject.SetActive(false); //InputField1
         selectionMenuTransform.GetChild(3).gameObject.SetActive(false); //ConnectorButton0
         selectionMenuTransform.GetChild(4).gameObject.SetActive(false); //ConnectorButton1
-        indicesOfInactiveMenuObjects[2] = true;
-        indicesOfInactiveMenuObjects[3] = true;
-        indicesOfInactiveMenuObjects[4] = true;
 
         //Set placeholder text of input
-        GameObject inputField0bject = selectionMenuTransform.GetChild(1);
-        GameObject placeholderObject = inputFieldObject.GetChild(0);
-        placeholderObject.GetComponent<Text>().text = "Mass (kg)";
+        Transform inputFieldObject0 = selectionMenuTransform.GetChild(1);
+        GameObject placeholderObject0 = inputFieldObject0.GetChild(0).gameObject;
+
+        //At creation, the inputfield generates a child GameObject at index 0 called InputField0 Input Caret
+        if (!placeholderObject0.CompareTag("UI-SelectionMenuInputPlaceholder"))
+            placeholderObject0 = inputFieldObject0.GetChild(1).gameObject;
+        placeholderObject0.GetComponent<Text>().text = "Mass (kg)";
     }
 
     //all 4 options
-    private void ConfigureSpringMenu() 
+    private void ConfigureSpringMenu()
     {
-        selectionMenuObject.SetActive(true);
         Transform selectionMenuTransform = selectionMenuObject.transform;
 
         //Set placeholder text of inputs
-        GameObject inputFieldObject0 = selectionMenuTransform.GetChild(1);
-        GameObject placeholderObject0 = inputFieldObject0.GetChild(0);
+        Transform inputFieldObject0 = selectionMenuTransform.GetChild(1);
+        GameObject placeholderObject0 = inputFieldObject0.GetChild(0).gameObject;
+        
+        //At creation, the inputfield generates a child GameObject at index 0 called InputField0 Input Caret
+        if (!placeholderObject0.CompareTag("UI-SelectionMenuInputPlaceholder"))
+            placeholderObject0 = inputFieldObject0.GetChild(1).gameObject;
         placeholderObject0.GetComponent<Text>().text = "Length (m)";
 
-        GameObject inputFieldObject1 = selectionMenuTransform.GetChild(1);
-        GameObject placeholderObject1 = inputFieldObject1.GetChild(0);
+
+        Transform inputFieldObject1 = selectionMenuTransform.GetChild(2);
+        GameObject placeholderObject1 = inputFieldObject1.GetChild(0).gameObject;
+        
+        //At creation, the inputfield generates a child GameObject at index 0 called InputField1 Input Caret
+        if (!placeholderObject1.CompareTag("UI-SelectionMenuInputPlaceholder"))
+            placeholderObject1 = inputFieldObject1.GetChild(1).gameObject;
         placeholderObject1.GetComponent<Text>().text = "Constant (N/m)";
+
+        UnityEngine.Debug.Log("Selected external force object, placeholder objects: " + placeholderObject0 + " and " + placeholderObject1);
+    }
+
+    public void ToButtonSelection()
+    {
+        isSelecting = true;
+        selectedGameObject = this.gameObject;
+        currentConnectionSelection = ToFromSelection.TO;
+    }
+
+    public void FromButtonSelection() 
+    {
+        isSelecting = true;
+        selectedGameObject = this.gameObject;
+        currentConnectionSelection = ToFromSelection.FROM;
     }
 }
