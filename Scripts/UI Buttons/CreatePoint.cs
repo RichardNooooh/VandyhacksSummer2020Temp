dfcd01f6e1561d73d2b1a6384fc50b1c564a4d3d
Ice <richard.jun.noh@gmail.com>
2020-07-12 06:30:39 -0500

    Rename prefabs and add Point object

---
+++ Scripts/UI Buttons/CreatePoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePoint : MonoBehaviour
+{
+    public GameObject refToPointObject;
+    public GameObject refToSimRoot;
+
+    public Vector3 creationPosition = new Vector3(0, 4.5f, 0);
+
+    public void OnClick() 
+    {
+        GameObject newCreatedPointObject = Instantiate(refToPointObject, creationPosition, Quaternion.identity);
+        newCreatedPointObject.transform.parent = refToSimRoot.transform;
+    }
+}
